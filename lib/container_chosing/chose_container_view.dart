dad564c2388199bd2a622bf5620476e11dc83f9b
Noor Ullah <nooullah178632@gmail.com>
2026-03-06 10:54:59 +0500

    new data

---
+++ lib/container_chosing/chose_container_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ChoseContainerView extends StatefulWidget {
+  const ChoseContainerView({super.key});
+
+  @override
+  State<ChoseContainerView> createState() => _ChoseContainerViewState();
+}
+
+class _ChoseContainerViewState extends State<ChoseContainerView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(children: [
+        
+      ],),
+    );
+  }
+}
