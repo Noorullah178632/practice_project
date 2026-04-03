69b73aadbb37cf3b1e9bcb6277e20913077ccafd
Noor Ullah <nooullah178632@gmail.com>
2026-04-03 14:19:52 +0500

    new data

--- lib/container_chosing/new_view.dart
+++ lib/container_chosing/new_view.dart
@@ -35,6 +35,15 @@ class _NewViewState extends State<NewView> {
             color: Colors.black,
             child: Text("this", style: TextStyle(color: Colors.white)),
           ),
+
+          Container(
+            padding: EdgeInsets.all(10),
+            height: 100,
+            width: 100,
+            color: Colors.black,
+            child: Text("new Test", style: TextStyle(color: Colors.white)),
+          ),
+
           Container(
             padding: EdgeInsets.all(10),
             height: 100,
